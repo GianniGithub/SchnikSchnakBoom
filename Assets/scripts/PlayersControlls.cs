0ed972f7c98191e280a460edfabdd23b11a49b77
Gianni <GianniGithub@github.com>
2022-08-16 01:10:26 +0200

    Input System Setup
    movements working now

--- Assets/scripts/PlayersControlls.cs
+++ Assets/scripts/PlayersControlls.cs
@@ -6,6 +6,12 @@ using static PlayerController;
 
 public class PlayersControlls : MonoBehaviour, IPlayer1Actions
 {
+    public float Speed;
+
+    Vector3 nextMove;
+    Rigidbody rb;
+
+
     public void OnMainShoot(InputAction.CallbackContext context)
     {
         Debug.Log("X Button");
@@ -13,18 +19,22 @@ public class PlayersControlls : MonoBehaviour, IPlayer1Actions
 
     public void OnMovment(InputAction.CallbackContext context)
     {
+        var moveInput = context.ReadValue<Vector2>();
+        nextMove = new Vector3(moveInput.x, 0, moveInput.y);
         Debug.Log("movment");
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        var test = new @PlayerController();
+        test.Player1.MainShoot.performed += OnMainShoot;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        rb.AddForce(nextMove * Speed, ForceMode.Force);
     }
 }
